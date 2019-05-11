82fb2fd9f35825235bb0f921375758cee10f865e
Nate Taylor <taylonr@gmail.com>
2019-05-11 18:00:30 -0500

    code formatting

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -9,7 +9,9 @@ import { Button, Welcome } from '@storybook/react/demo';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+    )
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
