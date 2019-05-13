fe91972ff0331049f26d8723633a6a390453187c
Nate Taylor <taylonr@gmail.com>
2019-05-12 20:39:47 -0500

    Adding markdown to the major banner

--- stories/banner.stories.js
+++ stories/banner.stories.js
@@ -5,6 +5,20 @@ import MajorBanner from '../components/major.banner';
 import MinorBanner from '../components/minor.banner';
 
 storiesOf('Components | Banners/Major', module)
+  .addParameters({
+    info: {
+      text: `
+        ### When to Use
+        This banner should be used, at most, once per page. When it is used, it should be placed at the top of the page, below the navigation bar. This banner is considered "Shouting", the focus is to grab the attention of the user before they have a chance to see anything else.
+        ___
+        **Location:**  1st element below navigation
+
+        **Max Quantity:** 1
+
+        **See Also:** Minor Banner
+      `,
+    },
+  })
   .add('With Only Title', () => (
     <MajorBanner title="Banner Title" photo="People Outdoors/shutterstock_116403520.jpg" />
   ))
