662a76ce7b9172a9cfb5d0acc978698bdb14542f
Terrence Mahnken <terrence.mahnken@hcahealthcare.com>
2021-02-19 15:12:27 -0600

    moves files and removes from master

--- components/form/cta-button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from './button';
-
-const CallToAction = props => (
-  <Button className="crf-button crf-button--call-to-action" {...props} />
-);
-
-export default CallToAction;
